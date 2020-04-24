3b6fd9c7777e4341b956bee0584f5708757055f5
Chua Choon Hoe <choonhoe@gmail.com>
2020-04-24 10:06:09 +0800

    smtp from heroku blocked by google revert to mailgun as mailer

--- instagram_web/util/sendmail.py
+++ instagram_web/util/sendmail.py
@@ -1,32 +1,41 @@
 import os
-# import requests
-import yagmail
+import requests
+# import yagmail
 from threading import Thread
 
-# mailgun_domain = os.environ.get("MAILGUN_DOMAIN")
+mailgun_domain = os.environ.get("MAILGUN_DOMAIN")
 mydomain = os.environ.get("MY_DOMAIN")
 
-sender_email=os.environ.get("sender_email")
-sender_password=os.environ.get("sender_password")
+# sender_email=os.environ.get("sender_email")
+# sender_password=os.environ.get("sender_password")
 
-yag=yagmail.SMTP(user=sender_email, password=sender_password)
+# yag=yagmail.SMTP(user=sender_email, password=sender_password)
+
+# def send_async_email(receiver_email, subject, contents):
+#     yag.send(receiver_email, subject, contents)
 
 def send_async_email(receiver_email, subject, contents):
-    yag.send(receiver_email, subject, contents)
+    return requests.post(
+        f"https://api.mailgun.net/v3/{mailgun_domain}/messages",
+        auth=("api", os.environ.get("MAILGUN_API_KEY")),
+        data={"from": f"Nextagram Admin <mailgun@{mailgun_domain}>",
+              "to": [f"{receiver_email}"],
+              "subject": f"{subject}",
+              "html": f"{contents}"})
 
 def send_email(sender, receiver_email, amount, image_url):
     subject="Congrats! You have received a donation"
-    contents="""<html><head></head><body><p>Your photo below have received a donation of USD{} from {} </p> <br> <img src="{}" style="width:100px;height:100px"></body></html>""".format(amount, sender, image_url)
+    contents="""<html><head></head><body><p>Your photo below have received a donation of USD{} from {} </p> <br> <img src="{}" style="width:200px;height:200px"></body></html>""".format(amount, sender, image_url)
     Thread(target=send_async_email, args=(receiver_email, subject, contents)).start()
 
 def send_email_follow(sender, receiver_email):
     subject=f"Request to follow from {sender}"
-    url=f'{mydomain}/follows/new'
+    url=f"{mydomain}/follows/new"
     contents="""<html><head></head><body><p>Please click on the link below to review and approve </p> <br> <a href="{}">Click To Approve</a></body></html>""".format(url)
     Thread(target=send_async_email, args=(receiver_email, subject, contents)).start()
 
 def send_email_approved(sender, receiver_email):
-    subject="Request approved"
+    subject="Follow request approved"
     contents=f"Your request to follow {sender} has been approved."
     Thread(target=send_async_email, args=(receiver_email, subject, contents)).start()
 
@@ -37,30 +46,5 @@ def send_email_reset(receiver_email, reset_token):
     Thread(target=send_async_email, args=(receiver_email, subject, contents)).start()
     
 
-# def send_email(sender, receiver_email, amount):
-#     return requests.post(
-#         f"https://api.mailgun.net/v3/{mailgun_domain}/messages",
-#         auth=("api", os.environ.get("MAILGUN_API_KEY")),
-#         data={"from": f"Nextagram Admin <mailgun@{mailgun_domain}>",
-#               "to": [f"{receiver_email}"],
-#               "subject": "Congrats!",
-#               "text": f"You have received a donation of USD{amount} from {sender}!"})
 
-# def send_email_follow(sender, receiver_email):
-#     return requests.post(
-#         f"https://api.mailgun.net/v3/{mailgun_domain}/messages",
-#         auth=("api", os.environ.get("MAILGUN_API_KEY")),
-#         data={"from": f"Nextagram Admin <mailgun@{mailgun_domain}>",
-#               "to": [f"{receiver_email}"],
-#               "subject": f"Follow Request from {sender}!",
-#               "text": f"Please click on the link to review and approve: {mydomain}/follows/new"})
-
-# def send_email_reset(receiver_email, reset_token):
-#     return requests.post(
-#         f"https://api.mailgun.net/v3/{mailgun_domain}/messages",
-#         auth=("api", os.environ.get("MAILGUN_API_KEY")),
-#         data={"from": f"Nextagram Admin <mailgun@{mailgun_domain}>",
-#               "to": [f"{receiver_email}"],
-#               "subject": "Password reset request received!",
-#               "text": f"Please click on the link to reset your password: {mydomain}/sessions/verify_token/{reset_token}"})
 
